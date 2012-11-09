18951781a382e51651acc19dcc335a9f9aa00bd3
Shikaga <shikaga@gmail.com>
2012-11-09 21:19:11 +0000

    Refactor

--- share.js
+++ share.js
@@ -35,7 +35,70 @@ function readDir(response, directory, string) {
 		response.write(fileLink, 'utf-8');
 	});
 	response.end();
-};
+}
+
+function handlePost(request, response) {
+    response.writeHead(200, { 
+        'Content-Type': 'text/html'
+	    ,'Access-Control-Allow-Origin': '*'});
+	request.content = "";
+	request.addListener("data", function(chunk) {
+	    request.content += chunk;
+	});
+	request.addListener("end", function(chunk) {
+	    console.log(querystring.parse(request.content).file, querystring.parse(request.content).body);
+		fs.writeFile(querystring.parse(request.content).file, querystring.parse(request.content).body, function(err) {
+    		if(err) {
+    		    console.log(err);
+    		    response.end("File Save failed. Is the file locked perhaps?", 'utf-8');
+    		} else {
+    		    console.log("The file was saved!");
+    		    response.end("Saved", 'utf-8');
+    		}
+    	}); 
+    });
+	response.end("POST RECEIVED!");  
+}
+
+function handleGet(request, response) {
+    var query = require('url').parse(request.url, true).query;
+    	if (query != null) {
+    		if (query.getFiles != null) {
+    				response.writeHead(200, { 
+    					'Content-Type': 'text/html'
+    					,'Access-Control-Allow-Origin': '*'});
+    				readDir(response, query.getFiles, "");
+    		} else if (query.getFile != null) {
+    			response.writeHead(200, { 
+    					'Content-Type': 'text/html'
+    					,'Access-Control-Allow-Origin': '*'});
+    			fs.readFile(query.getFile, 'utf8', 
+    				function(err, data) {
+    					console.log("DATA", data);
+    					response.end(data, 'utf-8');
+    				}
+    			)
+    			
+    		} else if (query.saveFile != null) {
+    			response.writeHead(200, { 
+    				'Content-Type': 'text/html'
+    				,'Access-Control-Allow-Origin': '*'});
+    			if (typeof query.line === 'string') {
+    				file = query.line;
+    			} else {
+    				file = query.line.join("\n");
+    			}			
+    		} else {
+    		    console.log("FAIL!", require('url').parse(request.url, true))
+    			response.writeHead(404);
+    			response.end();
+    		}
+		} else {
+		    console.log("FAIL2!")
+			response.writeHead(404);
+			response.end();		
+		}
+}
 
 var server = connect(
     connect.logger(),
@@ -46,76 +109,19 @@ var server2 = connect(
     connect.logger(),
 	function (request, response) {
 	    if (request.method == 'POST') {
-		response.writeHead(200, { 
-		    'Content-Type': 'text/html'
-		    ,'Access-Control-Allow-Origin': '*'});
-		request.content = "";
-		   request.addListener("data", function(chunk) {
-		    request.content += chunk;
-		   });
-	        request.addListener("end", function(chunk) {
-		    console.log(querystring.parse(request.content).file, querystring.parse(request.content).body);
-		    fs.writeFile(querystring.parse(request.content).file, querystring.parse(request.content).body, function(err) {
-			if(err) {
-			    console.log(err);
-			    response.end("File Save failed. Is the file locked perhaps?", 'utf-8');
-			} else {
-			    console.log("The file was saved!");
-			    response.end("Saved", 'utf-8');
-			}
-		    }); 
-		});
-		response.end("POST RECEIVED!");
+		    handlePost(request, response);  	
 	    } else if (request.method == 'GET') {
-		var query = require('url').parse(request.url, true).query;
-		if (query != null) {
-		if (query.getFiles != null) {
-				response.writeHead(200, { 
-					'Content-Type': 'text/html'
-					,'Access-Control-Allow-Origin': '*'});
-				readDir(response, query.getFiles, "");
-		} else if (query.getFile != null) {
-			response.writeHead(200, { 
-					'Content-Type': 'text/html'
-					,'Access-Control-Allow-Origin': '*'});
-			fs.readFile(query.getFile, 'utf8', 
-				function(err, data) {
-					console.log("DATA", data);
-					response.end(data, 'utf-8');
-				}
-			)
-			
-		} else if (query.saveFile != null) {
-			response.writeHead(200, { 
-				'Content-Type': 'text/html'
-				,'Access-Control-Allow-Origin': '*'});
-			if (typeof query.line === 'string') {
-				file = query.line;
-			} else {
-				file = query.line.join("\n");
-			}			
-		} else {
-		    console.log("FAIL!", require('url').parse(request.url, true))
-			response.writeHead(404);
-			response.end();
-		}
-		} else {
-		    console.log("FAIL2!")
-			response.writeHead(404);
-			response.end();		
-		}
+		    handleGet(request, response);
 	    }
 	}
-	);
+);
 
 var options = {db: {type: 'none'}, browserChannel: {cors: '*'}}; // See docs for options. {type: 'redis'} to enable persistance.
 
-// Attach the sharejs REST and Socket.io interfaces to the server
 sharejs.attach(server, options);
 
 port = parseInt(process.argv[2]);
 
-//server.listen(8000);
 http.createServer(server).listen(port);
 http.createServer(server2).listen(port+1);
 console.log('Server running at http://127.0.0.1:8000/');
